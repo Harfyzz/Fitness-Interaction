951609675e3f75c4c7c88a89c32f3668446e35e9
Afeez <harfyzz@gmail.com>
2024-08-14 07:57:43 +0100

    Initial Commit

---
+++ Fitness Interaction/Fitness_InteractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_InteractionApp.swift
+//  Fitness Interaction
+//
+//  Created by Afeez Yunus on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_InteractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
